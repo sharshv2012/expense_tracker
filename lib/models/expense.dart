369bb0358cf4811e8d609ccb37899cd176009567
Harsh <sharshvardhan759@gmail.com>
2023-10-02 14:09:40 +0530

    Commit 1 : used uuid for unique Id's and used enum for providing predefined set of values to the category variable in expense data model : )

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,13 +1,16 @@
 import 'package:uuid/uuid.dart';
 
 const uuid = Uuid(); // for generating unique ID's
+
+enum Category {leisure, food, work, travel }
 class Expense {
 
   Expense({required this.title, required this.amount,
-   required this.date }): id = uuid.v4();
+   required this.date, required this.category }): id = uuid.v4();
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
