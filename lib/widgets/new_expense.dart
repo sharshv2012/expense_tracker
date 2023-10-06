b23f42ec0e6d0e190005935316a2280a83e5b20a
Harsh <sharshvardhan759@gmail.com>
2023-10-06 20:45:37 +0530

    Commit 10: Took the category enum from category and mapped it into the dropDownButton items which accepts a list  also styled the elevated button : )

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,11 +1,13 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-final formatter = DateFormat('dd/MM/yyyy');// for formatting date
+final formatter = DateFormat('dd/MM/yyyy'); // for formatting date
+
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
 
-  @override 
+  @override
   State<StatefulWidget> createState() {
     return _NewExpenseState();
   }
@@ -15,22 +17,22 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
 
   void _presentDatePicker() async {
     final now = DateTime.now();
-    final firstDate = DateTime(now.year - 1, now.month, now.day); // previous possible date
+    final firstDate =
+        DateTime(now.year - 1, now.month, now.day); // previous possible date
     final lastDate = now;
 
     final pickedDate = await showDatePicker(
-      context: context, 
-      initialDate: now, 
-      firstDate: firstDate, 
-      lastDate: lastDate
-    );
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: lastDate);
     setState(() {
       _selectedDate = pickedDate;
     });
-    
   }
 
   @override
@@ -51,6 +53,7 @@ class _NewExpenseState extends State<NewExpense> {
       padding: EdgeInsets.all(16),
       child: Column(
         children: [
+           const SizedBox(height: 12,),
           TextField(
             controller: _titleController,
             maxLength: 50, //setting max string length
@@ -61,7 +64,8 @@ class _NewExpenseState extends State<NewExpense> {
           ),
           Row(
             children: [
-              Expanded( //Same rendering issue can arise having TextField inside of a row that's why used Expanded.
+              Expanded(
+                //Same rendering issue can arise having TextField inside of a row that's why used Expanded.
                 child: TextField(
                   controller: _amountController,
                   maxLength: 10, //setting max string length
@@ -73,32 +77,67 @@ class _NewExpenseState extends State<NewExpense> {
                   ),
                 ),
               ),
-              const SizedBox(width: 16,),
+              const SizedBox(
+                width: 16,
+              ),
               Expanded(
-                child: Row(// we are having a row inside of a row which can cause rendering problem that's why we are using Expanded.
-                  mainAxisAlignment: MainAxisAlignment.end, // pushing icon and text to the end
-                  crossAxisAlignment: CrossAxisAlignment.center, // for centering the item vertically
-                  children: [
-                    Text(_selectedDate == null ? "No Date Selected" : formatter.format(_selectedDate!)),
-                    IconButton(
+                  child: Row(
+                // we are having a row inside of a row which can cause rendering problem that's why we are using Expanded.
+                mainAxisAlignment:
+                    MainAxisAlignment.end, // pushing icon and text to the end
+                crossAxisAlignment: CrossAxisAlignment
+                    .center, // for centering the item vertically
+                children: [
+                  Text(_selectedDate == null
+                      ? "No Date Selected"
+                      : formatter.format(_selectedDate!)),
+                  IconButton(
                       onPressed: _presentDatePicker,
-                      icon: const Icon(Icons.calendar_month)
-                    ),
-                  ],
-                ) 
-                )
+                      icon: const Icon(Icons.calendar_month)),
+                ],
+              ))
             ],
+          ),
+          const SizedBox(
+            height: 16,
           ),
           Row(
             children: [
-              TextButton(onPressed: () {
-                Navigator.pop(context);
-              }, child: Text("CANCEL")),
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                    .map(
+                      (Category) => DropdownMenuItem(
+                        value: Category,
+                        child: Text(
+                          Category.name.toUpperCase(),
+                        ),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  setState(() {
+                    _selectedCategory = value!;
+                  });
+                },
+              ),
+              const Spacer(),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text("CANCEL")),
               ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                  borderRadius:  BorderRadius.circular(30),
+                  ),
+                ), 
                 onPressed: () {
                   print(_titleController.text);
                 },
                 child: const Text("SAVE"),
+                
               ),
             ],
           )
